d865eb99ec3be1f66c865c57c831a33621e3e64a
Fareed <fareed.ch7869@gmail.com>
2024-09-27 00:56:25 +0500

    (feat-frontend):Implement search and status filtered functionality in frontend

--- Frontend/TaskManagmentSystem/src/pages/Home.jsx
+++ Frontend/TaskManagmentSystem/src/pages/Home.jsx
@@ -1,8 +1,9 @@
 // Home Page
 import React, { useState, useEffect } from "react";
-import { getTasks, deleteTask } from "../services/api";
+import { getTasks, deleteTask, searchTasks, filteredTaskStatus } from "../services/api";
 import { useNavigate } from "react-router-dom";
 import Pagination from '../components/Pagination';
+import { debounce } from 'lodash';
 
 const Home = () => {
     const navigate = useNavigate();
@@ -10,26 +11,37 @@ const Home = () => {
     const [tasks, setTasks] = useState([]);
     const [currentPage, setCurrentPage] = useState(1);
     const [totalPages, setTotalPages] = useState(1);
-
+    const [loading, setLoading] = useState(true);
+    const [searchQuery, setSearchQuery] = useState('');
+    const [filteredStatus, setFilteredStatus] = useState(null);
 
     useEffect(() => {
-        fetchTasks(currentPage);
-    }, [currentPage]);
-
-    // const fetchTasks = async () => {
-    //     const taskList = await getTasks();
-    //     setTasks(taskList);
-    // };
+        if (filteredStatus) {
+            fetchFilteredTasks(currentPage);
+        } else {
+            fetchTasks(currentPage);
+        }
+    }, [currentPage, filteredStatus]);
 
     const fetchTasks = async (page) => {
+        setLoading(true);
         const taskData = await getTasks(page);
         setTasks(taskData.tasks);
         setTotalPages(taskData.totalPages);
-      };
+        setLoading(false);
+    };
+
+    const fetchFilteredTasks = async (page) => {
+        setLoading(true);
+        const taskData = await filteredTaskStatus(filteredStatus, page);
+        setTasks(taskData.tasks);
+        setTotalPages(taskData.totalPages);
+        setLoading(false);
+    };
 
     const handleDelete = async (id) => {
         await deleteTask(id);
-        fetchTasks();
+        fetchTasks(currentPage);
     };
 
     const handleView = (id) => {
@@ -38,93 +50,135 @@ const Home = () => {
 
     const handleUpdate = (id) => {
         navigate(`/updateTask/${id}`);
-      };
+    };
 
     const handlePageChange = (page) => {
         setCurrentPage(page);
+        if (filteredStatus) {
+            fetchFilteredTasks(page);
+        } else {
+            fetchTasks(page);
+        }
+    };
+
+    const debouncedSearch = debounce(async (query) => {
+        if (query) {
+            const taskData = await searchTasks(query, currentPage);
+            setTasks(taskData.tasks);
+            setTotalPages(taskData.totalPages);
+        } else {
+            fetchTasks(currentPage);
+        }
+    }, 300, { trailing: true });
+
+    const handleSearchChange = (event) => {
+        const query = event.target.value;
+        setSearchQuery(query);
+        debouncedSearch(query);
+    };
+
+    const handleFilterChange = (event) => {
+        const value = event.target.value;
+        setFilteredStatus(value);
     };
 
     return (
         <div className="bg-gray-900 min-h-screen py-10">
-            <div className="container mx-auto px-4 sm:px-6 lg:px-8">
-                <h1 className="text-4xl font-extrabold text-center text-white mb-8">
-                    Task Management System
-                </h1>
+          <div className="container mx-auto px-4 sm:px-6 lg:px-8">
+            <h1 className="text-4xl font-bold text-center text-white mb-10 tracking-tight">
+              Task Management System
+            </h1>
+      
+            <div className="flex flex-col sm:flex-row justify-between items-center mb-10 space-y-4 sm:space-y-0">
+              <button
+                onClick={() => navigate('/addTask')}
+                className="bg-gradient-to-r from-blue-500 to-blue-700 text-white font-semibold px-6 py-3 rounded-full shadow-lg hover:from-blue-600 hover:to-blue-800 transition duration-300 ease-in-out"
+              >
+                Add New Task
+              </button>
 
-                <button
-                    onClick={() => navigate('/addTask')}
-                    className="bg-blue-600 text-white px-6 py-3 rounded shadow hover:bg-blue-700 transition duration-300 mb-5 inline-block"
-                >
-                    Add New Task
-                </button>
-
-                <div className="overflow-x-auto">
-                    <div className="bg-gray-800 rounded-lg shadow-lg">
-                        <table className="min-w-full table-fixed">
-                            <thead>
-                                <tr className="text-white text-center">
-                                    <th className="py-3 border-b border-gray-600 w-5/12">
-                                        Title
-                                    </th>
-                                    <th className="py-3 border-b border-gray-600 w-3/12">
-                                        Status
-                                    </th>
-                                    <th className="py-3 border-b border-gray-600 w-3/12">
-                                        Actions
-                                    </th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                {tasks.map((task, index) => (
-                                    <tr
-                                        key={task.id}
-                                        className={`text-white text-center transition duration-300 ${task.status === "completed"
-                                                ? "bg-green-600"
-                                                : task.status === "in-progress"
-                                                    ? "bg-blue-600"
-                                                    : "bg-gray-500"
-                                            }`}
-                                    >
-                                        <td className="py-2 border-b border-gray-600">
-                                            {task.title}
-                                        </td>
-                                        <td className="py-2 border-b border-gray-600">
-                                            {task.status}
-                                        </td>
-                                        <td className="py-2 border-b border-gray-600">
-                                            <div className="flex justify-center space-x-2">
-                                                <button
-                                                    onClick={() => handleView(task.id)}
-                                                    className="bg-green-500 text-white px-3 py-1 rounded hover:bg-green-600 transition duration-300"
-                                                >
-                                                    View
-                                                </button>
-                                                <button
-                                                    onClick={() => handleUpdate(task.id)}
-                                                    className="bg-yellow-500 text-white px-3 py-1 rounded hover:bg-yellow-600 transition duration-300"
-                                                >
-                                                    Update
-                                                </button>
-                                                <button
-                                                    onClick={() => handleDelete(task.id)}
-                                                    className="bg-red-500 text-white px-3 py-1 rounded hover:bg-red-600 transition duration-300"
-                                                >
-                                                    Delete
-                                                </button>
-                                            </div>
-                                        </td>
-                                    </tr>
-                                ))}
-                            </tbody>
-                        </table>
-
-                        <Pagination currentPage={currentPage} totalPages={totalPages} onPageChange={handlePageChange} />
-
-                    </div>
-                </div>
+              <input
+                type="text"
+                value={searchQuery}
+                onChange={handleSearchChange}
+                placeholder="Search Tasks by title...."
+                className="flex-grow max-w-xs px-5 py-3 border border-gray-400 rounded-full shadow-lg focus:outline-none focus:ring-2 focus:ring-blue-500 transition duration-300 ease-in-out"
+              />
+    
+              <select
+                onChange={handleFilterChange}
+                className="bg-gray-800 text-white font-semibold px-6 py-3 border border-gray-600 rounded-full shadow-lg focus:outline-none focus:ring-2 focus:ring-blue-500 transition duration-300 ease-in-out"
+              >
+                <option value="">All Tasks</option>
+                <option value="completed">Completed</option>
+                <option value="in-progress">In Progress</option>
+                <option value="pending">Pending</option>
+              </select>
             </div>
+
+            {loading ? (
+              <div className="text-white text-center">Loading...</div>
+            ) : (
+              <div className="overflow-x-auto ">
+                <div className="bg-gray-800 rounded-lg shadow-lg">
+                  <table className="min-w-full table-fixed">
+                    <thead>
+                      <tr className="text-white text-center">
+                        <th className="py-3 border-b border-gray-600 w-2/12">#</th>
+                        <th className="py-3 border-b border-gray-600 w-3/12">Title</th>
+                        <th className="py-3 border-b border-gray-600 w-3/12">Status</th>
+                        <th className="py-3 border-b border-gray-600 w-4/12">Actions</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      {tasks.map((task, index) => (
+                        <tr
+                          key={task.id}
+                          className={`text-white text-center transition duration-300 ${task.status === "completed"
+                            ? "bg-green-600"
+                            : task.status === "in-progress"
+                              ? "bg-blue-600"
+                              : "bg-gray-500"
+                            }`}
+                        >
+                          <td className="py-2 border-b border-gray-600">{(currentPage - 1) * 5 + index + 1}</td>
+                          <td className="py-2 border-b border-gray-600">{task.title}</td>
+                          <td className="py-2 border-b border-gray-600">{task.status}</td>
+                          <td className="py-2 border-b border-gray-600">
+                            <div className="flex justify-center space-x-2">
+                              <button
+                                onClick={() => handleView(task.id)}
+                                className="bg-green-500 text-white px-3 py-1 rounded-full hover:bg-green-600 transition duration-300"
+                              >
+                                View
+                              </button>
+                              <button
+                                onClick={() => handleUpdate(task.id)}
+                                className="bg-yellow-500 text-white px-3 py-1 rounded-full hover:bg-yellow-600 transition duration-300"
+                              >
+                                Update
+                              </button>
+                              <button
+                                onClick={() => handleDelete(task.id)}
+                                className="bg-red-500 text-white px-3 py-1 rounded-full hover:bg-red-600 transition duration-300"
+                              >
+                                Delete
+                              </button>
+                            </div>
+                          </td>
+                        </tr>
+                      ))}
+                    </tbody>
+                  </table>
+      
+                  <Pagination currentPage={currentPage} totalPages={totalPages} onPageChange={handlePageChange} />
+                </div>
+              </div>
+            )}
+          </div>
         </div>
-    );
+      );
+      
 };
 
 export default Home;
--- Frontend/TaskManagmentSystem/src/services/api.js
+++ Frontend/TaskManagmentSystem/src/services/api.js
@@ -12,14 +12,16 @@ const API_URL = "http://localhost:3000/api/tasks";
 // };
 
 export const getTasks = async (page = 1, limit = 5) => {
-    try {
-      const response = await axios.get(`${API_URL}/getAllTask?page=${page}&limit=${limit}`);
-      return response.data.data;
-    } catch (error) {
-      console.error('Error fetching tasks:', error);
-      throw error;
-    }
-  };
+  try {
+    const response = await axios.get(
+      `${API_URL}/getAllTask?page=${page}&limit=${limit}`
+    );
+    return response.data.data;
+  } catch (error) {
+    console.error("Error fetching tasks:", error);
+    throw error;
+  }
+};
 
 export const deleteTask = async (id) => {
   try {
@@ -51,11 +53,37 @@ export const createTask = async (task) => {
 };
 
 export const updateTask = async (id, task) => {
-    try {
-      const response = await axios.put(`${API_URL}/updateTask/${id}`, task); // Update URL
-      return response.data.data;
-    } catch (error) {
-      console.error('Error updating task:', error);
-      throw error;
-    }
-  };
+  try {
+    const response = await axios.put(`${API_URL}/updateTask/${id}`, task); // Update URL
+    return response.data.data;
+  } catch (error) {
+    console.error("Error updating task:", error);
+    throw error;
+  }
+};
+
+export const searchTasks = async (query, page = 1, limit = 5) => {
+  try {
+    console.log("check query", query);
+    const response = await axios.get(
+      `${API_URL}/search?query=${query}&page=${page}&limit=${limit}`
+    );
+    return response.data.data;
+  } catch (error) {
+    console.error("Error searching tasks:", error);
+    throw error;
+  }
+};
+
+export const filteredTaskStatus = async (status, page = 1, limit = 5) => {
+  try {
+    console.log("check status api", status);
+    const response = await axios.get(
+      `${API_URL}/filteredTaskStatus?status=${status}&page=${page}&limit=${limit}`
+    );
+    return response.data.data;
+  } catch (error) {
+    console.error("Error while filtering task status:", error);
+    throw error;
+  }
+};
