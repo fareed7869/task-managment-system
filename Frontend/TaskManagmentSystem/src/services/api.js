22dadaab71732d7c069acd07aa497116e7ae1c17
Fareed <fareed.ch7869@gmail.com>
2024-09-26 19:24:17 +0500

    (feat-frontend):Implement pagination functionality for frontend

--- Frontend/TaskManagmentSystem/src/services/api.js
+++ Frontend/TaskManagmentSystem/src/services/api.js
@@ -1,15 +1,25 @@
 import axios from "axios";
 const API_URL = "http://localhost:3000/api/tasks";
 
-export const getTasks = async () => {
-  try {
-    const response = await axios.get(`${API_URL}/getAllTask`);
-    return response.data.data;
-  } catch (error) {
-    console.error("Error fetching tasks:", error);
-    throw error;
-  }
-};
+// export const getTasks = async () => {
+//   try {
+//     const response = await axios.get(`${API_URL}/getAllTask`);
+//     return response.data.data;
+//   } catch (error) {
+//     console.error("Error fetching tasks:", error);
+//     throw error;
+//   }
+// };
+
+export const getTasks = async (page = 1, limit = 5) => {
+    try {
+      const response = await axios.get(`${API_URL}/getAllTask?page=${page}&limit=${limit}`);
+      return response.data.data;
+    } catch (error) {
+      console.error('Error fetching tasks:', error);
+      throw error;
+    }
+  };
 
 export const deleteTask = async (id) => {
   try {
