1b839c47e34ae85d07ec61a5e078b97bd2b757a5
Fareed <fareed.ch7869@gmail.com>
2024-09-29 21:54:09 +0500

    (feat-frontend):Create Sign Up Page

--- Frontend/TaskManagmentSystem/src/App.jsx
+++ Frontend/TaskManagmentSystem/src/App.jsx
@@ -3,6 +3,7 @@ import Home from "./pages/Home";
 import TaskDetail from './pages/TaskDetail';
 import AddTask from './pages/AddTask';
 import UpdateTask from './pages/UpdateTask';
+import SignUpPage from "./pages/SignUp";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route path="/tasks/:id" element={<TaskDetail />} />
         <Route path="/addTask" element={<AddTask />} />
         <Route path="/updateTask/:id" element={<UpdateTask />} />
+        <Route path="/signup" element={<SignUpPage />} />
       </Routes>
     </Router>
   );
